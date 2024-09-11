9c2b78c53d4b2b3ab10244958937488f00207860
ajeigbepaul <pdave4krist@yahoo.com>
2024-09-11 19:14:20 +0100

    still figuring it

--- convex/podcasts.ts
+++ convex/podcasts.ts
@@ -92,9 +92,13 @@ export const getAllPodcasts = query({
 
 // this query will get the podcast by the podcastId.
 export const getPodcastById = query({
-  args: {podcastId: v.id("podcasts"),},
+  // args: {podcastId: v.id("podcasts"),},
+  args:{podcastId: v.optional(v.id("podcasts")) },
   handler: async (ctx, args) => {
     console.log("Received podcastId:", args.podcastId);
+    if (args.podcastId === undefined) {
+      return null;
+    }
     return await ctx.db.get(args.podcastId);
   },
 });
