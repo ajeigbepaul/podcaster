021f89e424a14f443942f2b7a7e2541223aa4cf9
ajeigbepaul <pdave4krist@yahoo.com>
2024-09-13 09:59:05 +0100

    added a prod key

--- app/provider/ConvexClerkProvider.tsx
+++ app/provider/ConvexClerkProvider.tsx
@@ -5,7 +5,8 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+// NEXT_PUBLIC_CONVEX_URL
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_SITE_URL as string);
 
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => (
   <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string} appearance={{
